927b3abce18f65f8d848886b2e25ba23b835fc80
justhuy23 <luxubu399@gmail.com>
2026-09-08 10:37:28 +0700

    Lần commit đầu tiên

---
+++ buoi1/inchuoi.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace buoi1
+{
+    class InChuoi
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Whoerver is happy will make others happy too.");
+            Console.WriteLine("--Anne Frank");
+            Console.ReadLine();
+        }
+    }
+}
